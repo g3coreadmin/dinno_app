cab48f27be18661299b4274c6758636627f40fe1
g3coreadmin <contatog3core@gmail.com>
2025-03-22 15:58:49 -0300

    Added new yml file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
 };
 
 export default nextConfig;
