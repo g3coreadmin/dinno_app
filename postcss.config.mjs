cdb9049a9391dcd726789cf299dfe9531fcf7e51
g3coreadmin <contatog3core@gmail.com>
2025-03-23 00:22:22 -0300

    changed postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,5 @@
-// postcss.config.js (modo ESM)
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-
 const config = {
-  plugins: [
-    tailwindcss({ engine: 'node' }), // 👈 aqui você força usar a versão JS e evita o erro do oxide
-    autoprefixer,
-  ],
-}
+  plugins: ["@tailwindcss/postcss"],
+};
 
-export default config
+export default config;
