0b0874f0ba24e1d012f90fa0cad7bb98939397f0
g3coreadmin <contatog3core@gmail.com>
2025-03-23 00:52:38 -0300

    changed postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,12 @@
+// postcss.config.mjs
 import tailwind from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
 
 export default {
   plugins: [
-    tailwind({ engine: 'node' }), // 👈 força uso do parser JS (sem oxide)
+    tailwind({
+      engine: 'node', 
+    }),
     autoprefixer,
   ],
 }
